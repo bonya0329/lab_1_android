46191ed96e1dea0c9fb33a4afcacec3f0da388a5
Bayan <bonya0329@gmail.com>
2020-09-24 11:14:14 +0600

    lab1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,32 +8,29 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView Header, Summary;
-    private EditText Weight;
+    private TextView text, summary;
+    private EditText weight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Header = findViewById(R.id.header);
-        Weight = findViewById(R.id.weight);
-        Summary = findViewById(R.id.summary);
+        text = findViewById(R.id.textView);
+        weight = findViewById(R.id.weight);
+        summary = findViewById(R.id.summary);
 
-        Weight.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+        weight.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 int ounces = Integer.parseInt(v.getText().toString());
                 double baseCost = 3.00, addedCost = 0.00;
 
-
-                // first Check
                 if(ounces > 30) {
                     baseCost = 4.00;
                 }
 
                 ounces -= 16;
-                // second Check
                 while(ounces > 0) {
                     ounces -= 4;
                     addedCost += 0.50;
@@ -44,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
                 report += "Base Cost: " + baseCost + "\n";
                 report += "Added Cost: " + addedCost + "\n";
                 report += "Total Shipping-Cost: " + total + "\n";
-                Summary.setText(report);
+                summary.setText(report);
                 return false;
             }
         });
